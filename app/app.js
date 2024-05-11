acd64c2cc674fe264f56961dbe56611983629637
Juan Ordoñez <jmosjroh06@gmail.com>
2024-05-11 15:44:49 -0600

    doin somethin

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import {pool} from "../db/db.js";
 const app = express()
 
 app.get('/', async function(req, res) {
-  const result = await pool.query('SELECT "hello world" as RESULT')
+  const result = await pool.query('SELECT * FROM Users')
   res.json(result)
 })
 
