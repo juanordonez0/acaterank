396d5f359b35424aee60c4d42dc3cbe3b46a6342
Juan Ordoñez <jmosjroh06@gmail.com>
2024-05-11 11:01:31 -0600

    ENVIROMENT PORT

--- app/app.js
+++ app/app.js
@@ -8,4 +8,4 @@ app.get('/', async function(req, res) {
   res.json(result)
 })
 
-app.listen(3060, () => console.log(`Server running on port 4000`));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
