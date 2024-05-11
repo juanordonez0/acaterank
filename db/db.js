b2d9eff6e210854d6b3ff65b731d986b582b9561
Juan Ordoñez <jmosjroh06@gmail.com>
2024-05-11 10:08:27 -0600

    added enviroment variables

--- db/db.js
+++ db/db.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: 'localhost',
-  password: 'jordonez',
-  user: 'root',
-  database: 'test',
-  port: 3306
+  host: process.env.HOST || 'localhost',
+  password: process.env.PASSWORD || 'jordonez',
+  user: process.env.USER || 'root',
+  database: process.env.DATABASE || 'test',
+  port: process.env.PORT || 3306
 });
