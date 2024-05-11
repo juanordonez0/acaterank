bccbeb4dc2fa79e6639bcf9426b1ccbe758b922f
Juan Ordoñez <jmosjroh06@gmail.com>
2024-05-11 17:59:19 -0600

    rows

--- app/app.js
+++ app/app.js
@@ -4,8 +4,8 @@ import {pool} from "../db/db.js";
 const app = express()
 
 app.get('/', async function(req, res) {
-  const result = await pool.query('SELECT * FROM Users')
-  res.json(result)
+  const [rows, fields]  = await pool.query('SELECT * FROM Users')
+  res.json(rows)
   
 })
 
